42f81c34a3fbc319660751ac3661979697b90a45
Vladimir Rybalsky <alevoldon@gmail.com>
2021-04-25 08:48:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   var mixedCase = 'I love you, Grandma.'
 if(string.toLowerCase() === mixedCase.toLowerCase()){
     console.log('I love you, too.')
-    return mixedCase;
+    return 'I love you, too.';
 }else if (string.toLowerCase() === string ) {
     console.log(lowercase);
     return lowercase;
